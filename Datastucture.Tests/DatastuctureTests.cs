80750ce378d3e2f87e58b137f58dad657c663be1
maurice <maurice@htl.do>
2025-10-03 08:02:48 +0200

    InsertBefore/After und PosOfElement

---
+++ Datastucture.Tests/DatastuctureTests.cs
@@ -0,0 +1,102 @@
+using Datastucture;
+using NUnit.Framework;
+using System;
+
+namespace Datastucture.Tests
+{
+    [TestFixture]
+    public class VerketteteListeTests
+    {
+        [Test]
+        public void AddLast_ShouldAddElements()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+
+            Assert.AreEqual(0, list.PosOfElement(1));
+            Assert.AreEqual(1, list.PosOfElement(2));
+        }
+
+        [Test]
+        public void InsertBefore_ShouldInsertElementCorrectly()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(1);
+            list.AddLast(3);
+            list.AddLast(5);
+
+            list.InsertBefore(3, 99);
+
+            Assert.AreEqual(1, list.PosOfElement(99));
+            Assert.AreEqual(2, list.PosOfElement(3));  
+        }
+
+        [Test]
+        public void InsertAfter_ShouldInsertElementCorrectly()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(1);
+            list.AddLast(3);
+            list.AddLast(5);
+
+            list.InsertAfter(3, 77);
+
+            Assert.AreEqual(2, list.PosOfElement(77)); 
+            Assert.AreEqual(1, list.PosOfElement(3));  
+        }
+
+        [Test]
+        public void PosOfElement_ShouldReturnMinusOne_WhenNotFound()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(10);
+            list.AddLast(20);
+
+            Assert.AreEqual(-1, list.PosOfElement(99));
+        }
+
+        [Test]
+        public void InsertBefore_OnHead_ShouldInsertAtBeginning()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(5);
+            list.AddLast(10);
+
+            list.InsertBefore(5, 1);
+
+            Assert.AreEqual(0, list.PosOfElement(1)); 
+            Assert.AreEqual(1, list.PosOfElement(5));
+        }
+
+        [Test]
+        public void InsertAfter_OnLastElement_ShouldAppend()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(5);
+            list.AddLast(10);
+
+            list.InsertAfter(10, 99);
+
+            Assert.AreEqual(2, list.PosOfElement(99)); 
+        }
+
+        [Test]
+        public void InsertBefore_ShouldThrow_WhenElementNotFound()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(1);
+
+            Assert.Throws<ArgumentException>(() => list.InsertBefore(99, 5));
+        }
+
+        [Test]
+        public void InsertAfter_ShouldThrow_WhenElementNotFound()
+        {
+            var list = new VerketteteListe<int>();
+            list.AddLast(1);
+
+            Assert.Throws<ArgumentException>(() => list.InsertAfter(99, 5));
+        }
+    }
+}
